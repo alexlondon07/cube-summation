76734d743cef32fdc58f0a9d58920f58ba06c742
Your Name <daehanpark@github.com>
2016-10-08 04:52:10 -0400

    ranges validation

--- app/Cube.php
+++ app/Cube.php
@@ -29,27 +29,29 @@ class Cube
     }
 
     function set_cell_value($x, $y, $z, $w){
-        $this->validate_coordinate($x,$y,$z);
+        $this->validate_params($x,$y,$z,$w);
         $this->cube[$x-1][$y-1][$z-1] = $w;
         return $w;
     }
 
     function get_cell_value($x, $y, $z){
-        $this->validate_coordinate($x,$y,$z);
+        $this->validate_params($x,$y,$z);
         return $this->cube[$x-1][$y-1][$z-1];
     }
 
-    private function validate_coordinate($x,$y,$z){
-        if ($this->validate_range($x) && 
-            $this->validate_range($y) && 
-            $this->validate_range($z)
-        ) return;
-
-        throw new Exception("Invalid Cube's Coordinate Value, must be 0 >= value <= N", 1);
-        
+    private function validate_params($x,$y,$z,$w=0){
+        return (
+            $this->validate_range($x,1,$this->n) && 
+            $this->validate_range($y,1,$this->n) && 
+            $this->validate_range($z,1,$this->n) &&
+            $this->validate_range($w,-1000000000,1000000000)
+        );
     }
 
-    private function validate_range($coordinate){
-        return  (($coordinate >= 0) && ($coordinate <= $this->n));
+    private function validate_range($value,$min,$max){
+        if  (($value >= $min) && ($value <= $max))
+            return true;
+
+        throw new Exception("Invalid cube's value: ".$value.", must be between ".$min." and ".$max, 1);
     }
 }
--- app/CubeSummationInputReader.php
+++ app/CubeSummationInputReader.php
@@ -4,6 +4,7 @@ namespace App;
 
 use Exception;
 use File;
+use App\CubeSummationInputReader as CIR;
 
 class CubeSummationInputReader
 {
@@ -13,12 +14,12 @@ class CubeSummationInputReader
     	$lines = explode("\n",$content);
     	$tests = [];
     	$t = $lines[0];
-
+    	CIR::validate_input($t,1,100);
     	for ($i=0, $line=1; $i < $t; $i++) {
     		try {
 	    		$n_m_line = explode(" ",$lines[$line++]);
-				$n = $n_m_line[0];
-				$m = $n_m_line[1];
+				$n = CIR::validate_input($n_m_line[0],1,100);
+				$m = CIR::validate_input($n_m_line[1],1,1000);
 				$cube_operations = [];
 			} catch (Exception $e) {
     			 	throw new Exception("Bad Input file, you are trying to execute more tests than defined in the variable T ", 1);
@@ -27,11 +28,8 @@ class CubeSummationInputReader
     		for ($j=0; $j < $m; $j++) {
     			
     			 	$params = explode(" ",$lines[$line+$j]);
-	    			//echo $lines[$line+$i]."<br>";
 					$cube_op = CubeOperationFactory::get_cube_operation($params);
-					$cube_operations[] = $cube_op;
-    			
-    			
+					$cube_operations[] = $cube_op;	
     		}
 
     		$tests[] = new CubeSummationTest($n,$cube_operations); 
@@ -40,4 +38,11 @@ class CubeSummationInputReader
 
 		return $tests;
     }
+
+    private static function validate_input($value, $min, $max){
+    	if ($value >= $min && $value <=$max)
+    			return $value;
+    	throw new Exception("Bad Input file, you are trying to use an invalid value ".$value, 1);
+    	
+    }	
 }
